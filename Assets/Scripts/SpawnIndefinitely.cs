eadf859df7516d6cda8e2bbf9fa7996194c125ec
chrismotian <chrisjberlin@googlemail.com>
2021-03-27 01:48:23 +0800

    add UAV with spawning indefinitely

---
+++ Assets/Scripts/SpawnIndefinitely.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnIndefinitely : MonoBehaviour {
+
+    //This is a generic script that can be used to spawn anything from power-ups to enemies!
+
+    [SerializeField] GameObject[] obj;
+    [SerializeField] float spawnMin = 1f;
+    [SerializeField] float spawnMax = 2f;
+
+	void Start () {
+        Spawn();
+	}
+	
+    void Spawn()
+    {
+        GameObject instance = (GameObject)Instantiate(obj[Random.Range(0, obj.GetLength(0))], transform.position, Quaternion.identity);
+		instance.transform.parent = this.transform;
+		Invoke("Spawn", Random.Range(spawnMin, spawnMax));    
+	}
+}
